8567baa86e80886852deb92cf714dff2f90164f7
Philix27 <philixbob@gmail.com>
2024-08-23 19:31:09 +0100

    shorten comps name

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from '@/comps/HelloWave';
+import ParallaxScrollView from '@/comps/ParallaxScrollView';
+import { ThemedText } from '@/comps/ThemedText';
+import { ThemedView } from '@/comps/ThemedView';
 
 export default function HomeScreen() {
   return (
--- comps/HelloWave.tsx
+++ comps/HelloWave.tsx
@@ -7,7 +7,7 @@ import Animated, {
   withSequence,
 } from 'react-native-reanimated';
 
-import { ThemedText } from '@/components/ThemedText';
+import { ThemedText } from '@/comps/ThemedText';
 
 export function HelloWave() {
   const rotationAnimation = useSharedValue(0);
